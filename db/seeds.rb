8b77c6f25319852749d898db519388a7cc35c7df
Oliver Attolini <attolini.oliver@gmail.com>
2024-05-22 16:14:34 +0100

    restaurant crud

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.create(name: "Tao te ching", address: "Pink street", phone_number: "+12 645 3769", category: "chinese");
+Restaurant.create(name: "Mamma mia", address: "Blbla street", phone_number: "+12 645 6665", category: "italian");
+Restaurant.create(name: "Steak a'shit", address: "Lincoln street", phone_number: "+12 645 9989", category: "french");
+Restaurant.create(name: "Waffleshit", address: "Ronaldo street", phone_number: "+12 645 7777", category: "belgian");
+Restaurant.create(name: "Zen", address: "Messi street", phone_number: "+12 645 3521", category: "japanese");
+Review.create(rating: 4, content: "good shit", restaurant_id: 2)
